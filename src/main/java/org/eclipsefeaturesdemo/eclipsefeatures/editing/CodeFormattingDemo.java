d531f6ac12a99e8a89724ae2871ed6177a767fa9
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-07-12 16:20:49 +0530

    Create CodeFormattingDemo.java

---
+++ src/main/java/org/eclipsefeaturesdemo/eclipsefeatures/editing/CodeFormattingDemo.java
@@ -0,0 +1,7 @@
+package org.eclipsefeaturesdemo.eclipsefeatures.editing;
+
+import java.util.List;
+
+public class CodeFormattingDemo{
+public static void main(String[] args){List<String> items=List.of("one","two","three");for(String item:items){if(item.length()>3){System.out.println(item.toUpperCase());}else{System.out.println(item);}}}
+}
