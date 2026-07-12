2d78e0bfc1e8502477650812808502be27fc393d
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-07-12 12:55:08 +0530

    Create UnusedImportsDemo.java

---
+++ src/main/java/org/eclipsefeaturesdemo/eclipsefeatures/editing/UnusedImportsDemo.java
@@ -0,0 +1,12 @@
+package org.eclipsefeaturesdemo.eclipsefeatures.editing;
+
+import java.util.List;
+import java.util.Map;
+import java.io.File;
+
+public class UnusedImportsDemo {
+
+    public static void greet() {
+        System.out.println("No imports are actually needed here.");
+    }
+}
