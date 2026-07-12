7e6238a792576f684ffc15eab53845901eaf6aa8
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-07-12 15:36:16 +0530

    Create DemoProductRunner.java

---
+++ src/main/java/org/eclipsefeaturesdemo/eclipsefeatures/editing/DemoProductRunner.java
@@ -0,0 +1,19 @@
+package org.eclipsefeaturesdemo.eclipsefeatures.editing;
+
+public class DemoProductRunner {
+	public static void main(String[] args) {
+
+        /*
+		DemoProduct a = new DemoProduct("SKU-1001", "Wireless Mouse", 24.99);
+        DemoProduct b = new DemoProduct("SKU-1001", "Wireless Mouse (Refurbished)", 14.99);
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("Equal?     " + a.equals(b));
+        System.out.println("Same hash? " + (a.hashCode() == b.hashCode()));
+        */
+    }
+
+
+}
