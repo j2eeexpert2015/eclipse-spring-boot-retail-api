2f255ae21a3bc3346f5c5b32137b2862409a8e92
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-07-12 14:41:13 +0530

    Create ProductCodeGenerationDemo.java

---
+++ src/main/java/org/eclipsefeaturesdemo/eclipsefeatures/editing/ProductCodeGenerationDemo.java
@@ -0,0 +1,9 @@
+package org.eclipsefeaturesdemo.eclipsefeatures.editing;
+
+public class ProductCodeGenerationDemo {
+
+    private String sku;
+    private String name;
+    private double price;
+
+}
