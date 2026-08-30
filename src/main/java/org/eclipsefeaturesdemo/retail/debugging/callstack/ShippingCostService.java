cc4e188be217b99259b651a63b6b9958603d0602
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2026-08-30 16:31:05 +0530

    Update ShippingCostService.java

--- src/main/java/org/eclipsefeaturesdemo/retail/debugging/callstack/ShippingCostService.java
+++ src/main/java/org/eclipsefeaturesdemo/retail/debugging/callstack/ShippingCostService.java
@@ -8,10 +8,20 @@ public class ShippingCostService {
     private static final double BASE_CHARGE = 5.0;
     private static final double CHARGE_PER_KILOGRAM = 2.0;
     private static final double CHARGE_PER_KILOMETRE = 0.1;
+    
+    public double calculate(double weightKg, double distanceKm) {
+        double shippingCost = BASE_CHARGE
+                + (weightKg * CHARGE_PER_KILOGRAM)
+                + (distanceKm * CHARGE_PER_KILOMETRE);
 
+        return shippingCost;
+    }
+
+    /*
     public double calculate(double weightKg, double distanceKm) {
         return BASE_CHARGE
                 + (weightKg * CHARGE_PER_KILOGRAM)
                 + (distanceKm * CHARGE_PER_KILOMETRE);
     }
+    */
 }
